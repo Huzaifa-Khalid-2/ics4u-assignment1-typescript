596c8ef080f2cac361271584e0fc07b7b8ccdde0
Huzaifa-Khalid-2 <Huzaifa-Khalid-2@users.noreply.github.com>
2022-09-20 23:03:04 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,23 @@
  * @since   2022-09-20
  */
 
-import promptSync from "prompt-sync"
+import promptSync from 'prompt-sync'
 const prompt = promptSync()
 
-const choices = ['rock', 'paper', 'scissors'];
-    console.log('This program is a rock paper scissor game')
-    const randomChoice = choices[Math.floor(Math.random() * 3)];
-    const choice = prompt("Enter rock, paper or scissors: ")
+const choices = ['rock', 'paper', 'scissors']
+console.log('This program is a rock paper scissor game')
+const randomChoice = choices[Math.floor(Math.random() * 3)]
+const choice = prompt('Enter rock, paper or scissors: ')
 
-    if (choice === randomChoice) {
-      console.log('Its a tie');
-    } else if (
-        (choice === 'rock' && randomChoice === 'paper') ||
-        (choice === 'paper' && randomChoice === 'scissors') ||
-        (choice === 'scissors' && randomChoice === 'rock')
-    ) {
-      console.log('You lost');
-    } else {
-      console.log('You won');
-    }
-    console.log("\nDone.")
+if (choice === randomChoice) {
+  console.log('Its a tie')
+} else if (
+  (choice === 'rock' && randomChoice === 'paper') ||
+  (choice === 'paper' && randomChoice === 'scissors') ||
+  (choice === 'scissors' && randomChoice === 'rock')
+) {
+  console.log('You lost')
+} else {
+  console.log('You won')
+}
+console.log('\nDone.')
